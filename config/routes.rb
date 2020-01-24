c64545627502a6e8a2433efe013e460b6ea7da97
imanwada <imaanwada@gmail.com>
2020-01-24 03:56:39 -0800

    fixed admin page error

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'pages/info'
 root to: redirect('/things')
   resources :things
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
